a3a88a33d7545f79345334b29f188bd322d59412
Chris Leichsenring <5677805+cleichsenring@users.noreply.github.com>
2020-05-13 20:21:07 -0400

    added search bar

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,6 @@ Chris Leichsenring
 
 //Global variables
 const galleryDiv = document.getElementById('gallery');
-const searchDiv = document.getElementsByClassName('search-container');
 const body = document.querySelector('body');
 
 let users = []
@@ -17,12 +16,28 @@ let users = []
 const randomAPI = fetch('https://randomuser.me/api/?results=12&nat=US')
   .then(res => res.json())
   .then(data => data.results.map(user => users.push(user)));
+  //.then(users.forEach(user => createCard(user)))
 
 
 //Gallery
 const createCard = user => {
+  const div = document.createElement('div');
+  div.className = 'card';
 
-
+  const data = `
+    <div class="card-img-container">
+      <img class="card-img" src="${user.picture.medium}" alt="profile picture">
+    </div>
+    <div class="card-info-container">
+      <h3 id="name" class="card-name cap">${user.name.first} ${user.name.last}</h3>
+      <p class="card-text">${user.email}</p>
+      <p class="card-text cap">${user.location.city}, ${user.location.state}</p>
+    </div>
+  `;
+  div.innerHTML = data;
+  galleryDiv.appendChild(div);
+  div.addEventListener('click', (e) => {
+  })
 }
 
 
@@ -39,7 +54,6 @@ const createModal = card => {
   div.className = 'modal';
   buttonDiv.className = 'modal-btn-container'
   
-  
   const data = `
     <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
     <div class="modal-info-container">
@@ -52,14 +66,15 @@ const createModal = card => {
       <p class="modal-text">${card.location.street.number} ${card.location.street.name}, ${card.location.city}, ${card.location.state} ${card.location.postcode}</p>
       <p class="modal-text">Birthday: ${birthday.toLocaleDateString()}</p>
     </div>
-  `;
+  `;//Update/fix State abbrev
   const buttons = `
     <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
     <button type="button" id="modal-next" class="modal-next btn">Next</button>
-  `
+  `;
 
   div.innerHTML = data;
   buttonDiv.innerHTML = buttons;
+
   container.appendChild(div);
   container.appendChild(buttonDiv);
   body.appendChild(container);
@@ -76,7 +91,17 @@ const createModal = card => {
 
 }
 
-//Search
+
+//Search bar
+const searchDiv = document.querySelector('.search-container');
+searchDiv.innerHTML = `
+<form action="#" method="get">
+  <input type="search" id="search-input" class="search-input" placeholder="Search...">
+  <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+</form>
+`;
 
 
+searchDiv.addEventListener('submit', e => {
 
+});
