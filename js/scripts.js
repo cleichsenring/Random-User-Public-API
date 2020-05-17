6994bd85a56fa8e7efdff6bdd4892e05aa9b819d
Chris Leichsenring <5677805+cleichsenring@users.noreply.github.com>
2020-05-17 15:19:57 -0400

    updated modal functions

--- js/scripts.js
+++ js/scripts.js
@@ -15,14 +15,21 @@ let users = []
 //Image, First Last Name, Email, City or location, cell, detailed address, birthday
 const randomAPI = fetch('https://randomuser.me/api/?results=12&nat=US')
   .then(res => res.json())
-  .then(data => data.results.map(user => users.push(user)));
-  //.then(users.forEach(user => createCard(user)))
+  .then(data => {
+    data.results.map(user => {
+      users.push(user);
+      createCard(user, users.length-1);
+    });
+    }
+  );
+  //.then(users.forEach((user, index) => createCard(user, index)))
 
 
 //Gallery
-const createCard = user => {
+const createCard = (user, index) => {
   const div = document.createElement('div');
   div.className = 'card';
+  div.setAttribute('user-index', index);
 
   const data = `
     <div class="card-img-container">
@@ -37,15 +44,15 @@ const createCard = user => {
   div.innerHTML = data;
   galleryDiv.appendChild(div);
   div.addEventListener('click', (e) => {
+    updateModal(users[index], index);
   })
 }
 
 
-//Modal
-const createModal = card => {
-  console.log(card)
-  const birthday = new Date(card.dob.date);
 
+
+//Modal
+const createModal = () => { 
   const container = document.createElement('div');
   const div = document.createElement('div');
   const buttonDiv = document.createElement('div');
@@ -53,20 +60,21 @@ const createModal = card => {
   container.className = 'modal-container';
   div.className = 'modal';
   buttonDiv.className = 'modal-btn-container'
+  container.style.display = 'none';
   
   const data = `
     <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-    <div class="modal-info-container">
-      <img class="modal-img" src="${card.picture.large}" alt="profile picture">
-      <h3 id="name" class="modal-name cap">${card.name.first} ${card.name.last}</h3>
-      <p class="modal-text">${card.email}</p>
-      <p class="modal-text cap">${card.location.city}</p>
+    <div class="modal-info-container" user-index="#">
+      <img class="modal-img" src="#" alt="profile picture">
+      <h3 id="name" class="modal-name cap"></h3>
+      <p id="email" class="modal-text"></p>
+      <p id="city" class="modal-text cap"></p>
       <hr>
-      <p class="modal-text">${card.cell}</p>
-      <p class="modal-text">${card.location.street.number} ${card.location.street.name}, ${card.location.city}, ${card.location.state} ${card.location.postcode}</p>
-      <p class="modal-text">Birthday: ${birthday.toLocaleDateString()}</p>
+      <p id="phone" class="modal-text"></p>
+      <p id="address" class="modal-text"></p>
+      <p id="dob" class="modal-text"></p>
     </div>
-  `;//Update/fix State abbrev
+  `;
   const buttons = `
     <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
     <button type="button" id="modal-next" class="modal-next btn">Next</button>
@@ -80,15 +88,27 @@ const createModal = card => {
   body.appendChild(container);
 
 //Event listeners
-  document.getElementById('modal-close-btn').addEventListener('click', e => container.remove());
-  
-  document.getElementById('modal-prev').addEventListener('click', e => {
-    console.log('Modal PREV event listener working');
-  });
-  document.getElementById('modal-next').addEventListener('click', e => {
-    console.log('Modal NEXT event listener working');
+  document.getElementById('modal-close-btn').addEventListener('click', e => {
+    container.style.display = 'none';
   });
 
+}
+createModal();
+
+
+const updateModal = (user, index) => {
+  document.querySelector('.modal-container').style.display = ''
+
+  const birthday = new Date(user.dob.date);
+  document.querySelector('.modal-img').src = user.picture.large;
+  document.querySelector('.modal-name').innerHTML = `${user.name.first} ${user.name.last}`;
+  document.querySelector('#email').innerHTML = user.email;
+  document.querySelector('#city').innerHTML = user.location.city;
+  document.querySelector('#phone').innerHTML = user.cell;
+  document.querySelector('#address').innerHTML = `${user.location.street.number} ${user.location.street.name} <br> ${user.location.city}, ${user.location.state} ${user.location.postcode}`
+//Update/fix State abbrevse
+  document.querySelector('.modal-info-container').setAttribute('user-index', index )
+  document.querySelector('#dob').innerHTML = `Birthday: ${birthday.toLocaleDateString()}`;
 }
 
 
@@ -101,6 +121,16 @@ searchDiv.innerHTML = `
 </form>
 `;
 
+  //Event listeners
+
+document.getElementById('modal-prev').addEventListener('click', () => {
+  const currentIndex = parseInt(document.querySelector('.modal-info-container').getAttribute('user-index'), 10)
+  currentIndex === 0 ? updateModal(users[users.length  - 1], users.length - 1) : updateModal(users[currentIndex - 1] , currentIndex - 1);
+});
+document.getElementById('modal-next').addEventListener('click', () => {
+  const currentIndex = parseInt(document.querySelector('.modal-info-container').getAttribute('user-index'), 10);
+  currentIndex === users.length - 1 ? updateModal(users[0], 0) : updateModal(users[currentIndex + 1] , currentIndex+1);
+});
 
 searchDiv.addEventListener('submit', e => {
 
